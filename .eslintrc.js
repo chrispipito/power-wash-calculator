9c6acf9d013f416dc311deb40cff73ff8e318b59
Chris Pipito <cpipito29@gmail.com>
2024-10-08 10:50:40 -0400

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+        es2020: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        // You can add specific rules here
+    },
+};
